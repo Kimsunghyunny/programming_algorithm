79df0784cc177095a7ac20de14b7ee4172056199
kimsunghyun <sorkaj123@gmail.com>
2020-09-23 22:54:49 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "hello! c++" << endl;
+
+    int tmp = 0;
+
+    for (int i =0; i <5; i++) {
+        cout << tmp <<endl;
+        tmp++;
+    }
+
+    return 0;
+}
