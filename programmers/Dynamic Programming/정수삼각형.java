c20cd36b391631e3bebd16962378019861e65018
kimsunghyun <sorkaj123@gmail.com>
2021-05-27 15:47:34 +0900

    dp

---
+++ programmers/Dynamic Programming/정수삼각형.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        
+        int[][] dp = new int[triangle.length][triangle.length];
+        
+        // 다이나믹 프로그래밍 알고리즘. dp[i][j] = triangle[i][j] + 연결된 좌우 중 더 큰 dp값
+        
+        dp[0][0] = triangle[0][0];
+        
+        for(int i=1; i< triangle.length; i++) {
+            dp[i][0] = triangle[i][0] + dp[i-1][0];
+            
+            for(int j=1; j<i+1; j++) {
+                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]);
+            }
+        }
+        
+        
+        int max = 0;
+        for(int i=0; i< dp.length; i++) {
+            if(max < dp[dp.length-1][i])
+                max = dp[dp.length-1][i];
+        }
+        return max;
+        
+        
+        /*
+        Arrays.sort(dp[dp.length-1]);
+        
+        return dp[dp.length-1][dp.length-1];
+        */
+    }
+}
