bc7b21b47cf5e3b51e6ad189cb2048fc4e2ae668
kimsunghyun <sorkaj123@gmail.com>
2021-06-03 15:36:45 +0900

    주석 변경

--- baekjoon/재귀/별찍기10.py
+++ baekjoon/재귀/별찍기10.py
@@ -6,7 +6,8 @@ def star(n):
     if n == 3:
         Map[0][:3] = Map[2][:3] = [1]*3
         Map[1][0] = Map[1][2] = 1
-        return
+        return # return을 하지 않으면 recursion 에러가 발생한다. 
+        #python에서 정의한것보다 재귀가 더 깊게 들어가 생기는 오류로 , 끝이나지 않기 때문에 발생하는것같다.
 
     a = int(n/3) # python에서는 정수/정수 해도 float이 나오니, 정수 값을 원하면 int함수를 이용하거나 //을 이용하자
     star(a)
