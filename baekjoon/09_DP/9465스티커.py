2bea46b14592c40427d62c502ef808d71ebe609a
Kimsunghyunny <sorkaj123@gmail.com>
2021-07-27 18:17:51 +0900

    초기화에 대해서 배열 사이즈 고려해서 선언해줘야하는 것 잊지말기

--- baekjoon/09_DP/9465스티커.py
+++ baekjoon/09_DP/9465스티커.py
@@ -6,8 +6,10 @@ T = int(si())
 for _ in range(T):
     n = int(si())
     a = [list(map(int,si().split())) for _ in range(2)]
-    a[0][1] += a[1][0]
-    a[1][1] += a[0][0]
+    #  n이 1인 경우에는 2번재 값인 a[0][1]과 a[1][1]을 초기화 할 수 없다.
+    if 2 <= n:
+        a[0][1] += a[1][0]
+        a[1][1] += a[0][0]
     for i in range(2,n):
         a[0][i] += max(a[1][i-1],a[1][i-2])
         a[1][i] += max(a[0][i-1],a[0][i-2])
