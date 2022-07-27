d000baa21a390b7be903d88ab2888f99ae6e7f23
sunghyun <sorkaj123@gmail.com>
2022-07-27 21:51:26 +0900

    수정

--- baekjoon/04_TwoPointer/2473세용액.py
+++ baekjoon/04_TwoPointer/2473세용액.py
@@ -6,22 +6,23 @@ lst = list(map(int, si().split()))
 lst.sort()
 
 v1, v2, v3 = 0, 0, 0
-bestSum = 1<<31
+bestSum = 3*(10**9)  # long으로 선언 x시 틀림
+flag = 0
 for i in range(n-2):
-    val = lst[i]
     l, r = i+1, n-1
     while l < r:
-        nowSum = val + lst[l] + lst[r]
-        if abs(bestSum) > abs(nowSum):
-            bestSum = nowSum
-            v1 = val
+        nowSum = lst[i] + lst[l] + lst[r]
+        if bestSum > abs(nowSum):
+            bestSum = abs(nowSum)
+            v1 = lst[i]
             v2 = lst[l]
             v3 = lst[r]
         if nowSum > 0: r-=1
         elif nowSum < 0 : l+=1
-        else: break
+        else: 
+            flag = 1
+            break
+
+    if flag : break
 
 print(v1, v2, v3) 
-
-
-# 시간초과남
